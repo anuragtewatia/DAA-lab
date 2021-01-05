a178d152adbe687fe05349c587914ea15c8cdcd9
anuragtewatia <72623745+anuragtewatia@users.noreply.github.com>
2021-01-05 12:31:50 +0530

    Update Insertion sort.c

--- Insertion sort.c
+++ Insertion sort.c
@@ -13,7 +13,7 @@ void main()
         scanf("%d",&a[i]);
     }
 
-    for(i=1;i<=n;i++)
+    for(i=1;i<n;i++)
     {
         j=i;
         while(j>0 && a[j-1]>a[j])
@@ -26,7 +26,7 @@ void main()
         }
     }
     printf("Sorted list is as follows\n");
-    for(i=0;i<=n;i++)
+    for(i=0;i<n;i++)
     {
         printf("%d ",a[i]);
     }
